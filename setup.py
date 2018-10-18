1482b8eb1ffd7e32f0d976a5d0657a3e9b7ec8d3
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-18 21:06:18 +0200

    add pip installer

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from os.path import isfile
 
 __name__ = 'ansible-extras'
 cwd = os.getcwd()
-print (cwd)
-base = '/etc/ansible/roles/%s' % __name__.split('-')[1]
+base = __name__
 data_files = []
 for dir in ['library', 'meta', 'filter_plugins']:
   files = []
@@ -17,11 +16,19 @@ for dir in ['library', 'meta', 'filter_plugins']:
     files.append("%s/%s" % (dir,file))
   data_files.append(("%s/%s" % (base, dir), files))
 
+
+class link_role(install):
+    def run(self):
+        install.run(self)
+        print (self)
+
+
 setup(
     name = __name__,
     version = __version__,
     packages = [__name__.replace("-", "_")],
     data_files = data_files,
+    cmdclass = {'install': link_role},
     url = 'https://www/github.com/moshloop/ansible-extras',
     author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com'
 )
